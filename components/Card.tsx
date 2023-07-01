ca5e848c93172992a9563d602a7f5a1d065bdb04
Vincent Escoffier <vincent.escoffier1@gmail.com>
2023-07-01 17:48:52 +0200

    fix

--- components/Card.tsx
+++ components/Card.tsx
@@ -31,11 +31,9 @@ function getPosition(element: HTMLElement | null) {
   let xPosition = 0;
   let yPosition = 0;
 
-  while (element) {
-    xPosition += element.offsetLeft - element.scrollLeft + element.clientLeft;
-    yPosition += element.offsetTop - element.scrollTop + element.clientTop;
-    element = element.offsetParent as HTMLElement;
-  }
+  xPosition += element.offsetLeft - element.scrollLeft + element.clientLeft;
+  yPosition += element.offsetTop - element.scrollTop + element.clientTop;
+
   return {
     x: xPosition,
     y: yPosition,
